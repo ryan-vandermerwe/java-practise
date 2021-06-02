77fe2354f8cfec78590b361964f25c031d4c5387
DESKTOP-NAAN1N3\jewjeen <jewjeen@gmail.com>
2021-06-02 19:23:09 +0200

    message

--- src/com/company/Practise.java
+++ src/com/company/Practise.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Practise {
-
+//
     public static void main(String[] args) {
         System.out.println("You had me at 'Hello world'");
         System.out.println("Genetjie");
